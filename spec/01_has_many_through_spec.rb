f8b5374f80b446bc9b74b3551e4b88e917c9c2bf
Alice Brunel <alc.brunel@gmail.com>
2019-08-03 14:59:51 +0000

    Automatically backed up by Learn

--- spec/01_has_many_through_spec.rb
+++ spec/01_has_many_through_spec.rb
@@ -215,5 +215,25 @@ describe "Object relationships" do
       end
     end
     
+    describe "#most_frequent" do
+      it "returns the Customer instance that is the most frequent" do
+        howard = Customer.new("Howard", 30)
+        daniel = Customer.new("Daniel", 30)
+        lisa = Customer.new("Lisa", 27)
+        josh = Customer.new("Josh", 31)
+        steven = Customer.new("Steven", 28)
+        terrance = Waiter.new("Terrance", 1)
+        
+        
+        howard.new_meal(terrance, 15, 2)
+        daniel.new_meal(terrance, 15, 4)
+        lisa.new_meal(terrance, 15, 5)
+        josh.new_meal(terrance, 15, 1)
+        steven.new_meal(terrance, 15, 3)
+
+        expect(terrance.worst_tipper).to eq(josh)
+      end
+    end
+        
   end
 end
