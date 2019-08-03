1e59cfd84365977b5a58d71302fd98f495f99971
Alice Brunel <alc.brunel@gmail.com>
2019-08-03 15:29:13 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -50,4 +50,13 @@ class Customer
     oldest_customer
   end
   
+  def most_frequent_customer
+    arr = []
+    meals.each do |meal|
+      arr << meal.customer.name
+    end
+    most_freq = arr.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    arr.max_by { |v| most_freq[v] }
+  end
+  
 end
